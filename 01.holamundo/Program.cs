5ee78b28eb9b1ac623e7bc94088d0f7a16bd3f30
Ulthem <luisma-117@outlook.com>
2020-01-30 11:20:07 -0600

    va todo otra vez

---
+++ 01.holamundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.holamundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bieve¿¿hsfghfsadkgj");
+        }
+    }
+}
