46d61b9fc9120d6160093946532df8313bb630b7
Ulthem <luisma-117@outlook.com>
2020-01-30 11:50:38 -0600

    va todo otra vez

---
+++ 03.areatriangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03.areatriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bas, altura, area;
+
+            Console.WriteLine("Programa que calcula el area del triagulo");
+
+            Console.WriteLine("Dame la base: ");
+            bas=double.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la altura: ");
+            altura=double.Parse(Console.ReadLine());
+
+            area=(bas*altura)/2;
+            
+            Console.WriteLine($"El area es {area}");
+            Console.ReadLine();           
+        }
+    }
+}
