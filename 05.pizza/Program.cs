e91732b32c04d03609576ea1a138370080e6a607
Ulthem <luisma-117@outlook.com>
2020-02-04 11:43:01 -0600

    va de nuez

---
+++ 05.pizza/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _05.pizza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length==0){
+                Menu();
+                //return 1;
+            }
+
+            char tam=char.Parse(args[0]);
+            string[] ings=args[1].Split("+");
+            char cub=char.Parse(args[2]);
+            char don=char.Parse(args[3]);
+
+            string tamaño="", ingredientes="", cubierta="", donde="";
+            
+            if(tam=='p') tamaño="Pequeña";
+            else if(tam=='M') tamaño="Mediana";
+            else tamaño="Grande";
+
+            foreach (string i in ings){
+                switch (char.Parse(i))
+                {
+                   case 'E': ingredientes+= "Extra Queso, ";break;
+                   case 'C': ingredientes+= "Champiñones, ";break;
+                   case 'T': ingredientes+= "Tomates, ";break;
+                   case 'P': ingredientes+= "Piña, ";break;
+                   
+                }
+            }
+
+            if(cub=='D') cubierta="Delgada"; else cubierta="Gruesa";
+
+            if(don=='C') donde="Comer aqui"; else donde="Para llevar";
+
+            Console.Clear();
+            Console.WriteLine($"Tamaño      : {tamaño}");
+            Console.WriteLine($"Ingredientes: {ingredientes}");
+            Console.WriteLine($"Cubierta    : {cubierta}");
+            Console.WriteLine($"Comer donde : {donde}. \n");
+
+            
+
+            //return 0;
+        }
+
+        static void Menu(){
+            Console.WriteLine("Procesando Pizza \n");
+            Console.WriteLine("Tamaño      : (P)equeña,     (M)ediana o    (G)rande");
+            Console.WriteLine("Ingredientes: (E)xtra queso, (C)hampiñones, (T)omates, (P)iña");
+            Console.WriteLine("Cubierta    : (D)elgada o    (G)ruesa");
+            Console.WriteLine("Comer donde : (C)omer aqui o (P)ara llevar \n");
+            Console.WriteLine("<Tamaño> <ing1+ing2...> <Cubierta> <Donde>");
+
+
+
+
+        }
+    }
+}
