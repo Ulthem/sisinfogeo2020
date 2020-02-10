552d356085c81a191345f186eef1e7fbc5acf90e
Ulthem <luisma-117@outlook.com>
2020-02-10 15:17:36 -0600

    va de nuez

---
+++ 09.vectorcubo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09.vectorcubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Max=30;
+
+            double[] A = new double[Max];
+            double[] B = new double[Max];
+
+            Random aleatorio = new Random();
+
+            for(int i=0; i<Max; i++){
+                A[i] = aleatorio.Next(1,100);
+                
+                B[i] = Math.Pow(A[i],3);
+             }
+
+
+             Console.WriteLine("Eleva al cubo los 30 elementos del vector\n ");
+
+
+             Imprime(A);
+             Console.WriteLine();
+             Imprime(B);
+             Console.WriteLine();
+
+           
+        }
+
+        static void Imprime(double [] v){
+            for(int i=0; i<v.Length; i++){
+                Console.Write($"{v[i]} ");
+                
+            }Console.WriteLine();
+        }
+    }
+}
